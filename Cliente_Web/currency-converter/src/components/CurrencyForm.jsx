d81c71b63a4343f10a4ea5c6bd4c11349cd0fc74
DarielB <darielbezerra13@gmail.com>
2025-06-20 23:52:59 -0300

    React Organization

--- Cliente_Web/currency-converter/src/components/CurrencyForm.jsx
+++ Cliente_Web/currency-converter/src/components/CurrencyForm.jsx
@@ -5,18 +5,23 @@ const CurrencyForm = ({ from, to, amount, setFrom, setTo, setAmount, onConvert }
     <div className="form-group">
       <label>De:</label>
       <select value={from} onChange={(e) => setFrom(e.target.value)}>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="BTC">BTC</option>
+        <option value="BRL">Real</option>
+        <option value="USD">Dólar Americano</option>
+        <option value="EUR">Euro</option>
+        <option value="BTC">Bitcoin</option>
+        <option value="CHF">Franco-Suiço</option>
+        <option value="JPY">Iene Japonês</option>
       </select>
     </div>
 
     <div className="form-group">
       <label>Para:</label>
       <select value={to} onChange={(e) => setTo(e.target.value)}>
-        <option value="BRL">BRL</option>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
+        <option value="BRL">Real</option>
+        <option value="USD">Dólar Americano</option>
+        <option value="EUR">Euro</option>
+        <option value="BTC">Bitcoin</option>
+        <option value="JPY">Iene Japonês</option>
       </select>
     </div>
 
